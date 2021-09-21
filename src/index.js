0153a3cf84a16ca2e21bc55b6bff0454dde4862d
KatIsCoding <fabrigoru@gmail.com>
2021-09-21 11:29:41 -0600

    Basic webpack setup

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import _ from "lodash";
+import "./style.css"
+console.log("Hello world")
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const htmlwebpack = require("html-webpack-plugin")
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  mode: "development",
+  devServer: {
+      static: "./dist",
+  },
+  plugins: [
+      new htmlwebpack({
+        template: "./src/index.html"
+      })
+  ],
+  module: {
+      rules: [
+          {
+              test: /\.css$/i,
+              use: ["style-loader", "css-loader"],
+          },
+      ],
+  },
+};
