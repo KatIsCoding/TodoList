51abb8b8aecce8c2bdecdd52abfafbfbea5e6e67
Snow!! <fabrigoru@gmail.com>
2021-09-21 23:27:12 -0600

    Sort the array to the correct order

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import _ from 'lodash'; // Unused vars disabled for lodash
 import './style.css';
 import Task from './taskClass.js';
 
-const dataBase = [new Task('Description test1', 0), new Task('Description test2', 1), new Task('Description test3', 2)];
+const dataBase = [new Task('Description test1', 0, false), new Task('Description test2', 1, false), new Task('Description test3', 2, false)];
 
 function showTasks() {
+  dataBase.sort((first, second) => {return first.index - second.index})
   const container = document.getElementById('todo-container');
   dataBase.forEach((task) => {
     const taskElement = document.createElement('li');
