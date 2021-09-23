6a2b43d0beabc53b6a0c0fcbc6aae782e3e5be7f
KatIsCoding <fabrigoru@gmail.com>
2021-09-23 16:27:00 -0600

    Fall back of recent Push

---
+++ src/add-remove.js
@@ -0,0 +1,6 @@
+import { dataBase, updateValues } from "./functions.js";
+
+function addTask(task){
+    dataBase.push(task)
+}
+
--- src/index.js
+++ src/index.js
@@ -3,35 +3,39 @@ import _ from 'lodash'; // Unused vars disabled for lodash
 import './style.css';
 import { changeCompletedState, dataBase, loadValues } from './functions.js';
 
+function renderTask(task){
+  const container = document.getElementById('todo-container');
+  const taskElement = document.createElement('li');
+  taskElement.classList.add('todo-element');
+  const completed = document.createElement('input');
+  completed.type = 'checkbox';
+  completed.checked = task.completed;
+  completed.addEventListener('change', () => {
+    changeCompletedState(task);
+  });
+  taskElement.appendChild(completed);
+  const description = document.createElement('p');
+  description.innerText = task.description;
+  taskElement.appendChild(description);
+  const moveicon = document.createElement('span');
+  moveicon.classList.add('material-icons');
+  moveicon.innerText = 'more_vert';
+  taskElement.appendChild(moveicon);
+  container.appendChild(taskElement);
+}
+
+
 function showTasks() {
   dataBase.sort((first, second) => first.index - second.index);
-  const container = document.getElementById('todo-container');
   dataBase.forEach((task) => {
-    const taskElement = document.createElement('li');
-    taskElement.classList.add('todo-element');
-
-    const completed = document.createElement('input');
-    completed.type = 'checkbox';
-    completed.checked = task.completed;
-    completed.addEventListener('change', () => {
-      changeCompletedState(task);
-    });
-    taskElement.appendChild(completed);
-
-    const description = document.createElement('p');
-    description.innerText = task.description;
-    taskElement.appendChild(description);
-
-    const moveicon = document.createElement('span');
-    moveicon.classList.add('material-icons');
-    moveicon.innerText = 'more_vert';
-    taskElement.appendChild(moveicon);
-
-    container.appendChild(taskElement);
+    renderTask(task)
   });
 }
 
 window.onload = () => {
   loadValues();
   showTasks();
+  /* Add event listener to New Task Input */
+  document.getElementById("addTask").addEventListener("submit", () => {console.log("It worked bro")})
+
 };
